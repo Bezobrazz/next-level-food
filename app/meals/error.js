5991240fd1e604cbb7f26b711d09e12cc7e44a19
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-07-16 17:14:28 +0000

    Initial commit

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An Error occured!</h1>
+      <p>Feaild to fatch meal data. Please, try again later</p>
+    </main>
+  );
+}
